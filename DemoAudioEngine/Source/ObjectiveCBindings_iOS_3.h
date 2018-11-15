d5e1c8cc7a41317702af35802348f08609c9c94d
Adam Wilson <adam@codegarden.co.uk>
2018-11-15 22:48:04 +0000

    Seperate iOS bindings

---
+++ DemoAudioEngine/Source/ObjectiveCBindings_iOS_3.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#ifdef __APPLE__
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>             // 2
+
+typedef void (^Callback)();         // 3
+
+@interface DemoAudioEngineBindings : NSObject
+
+- (void) play: (NSString*) urlString;
+- (void) stop;
+
+- (void) pause;
+- (void) resume;
+
+- (void) setRoomSize: (float) roomSize;
+- (void) setLowpassCutoff: (float) cutoff;
+
+- (void) setPlaybackDidFinish: (Callback) callback;       // 3
+
+- (void) setWaveformComponentBounds: (CGRect) bounds;               // 2
+- (void) addWaveformComponentToView: (UIView*) viewToAttachTo;      // 2
+- (void) removeWaveformComponentFromView;						    // 2
+
+@property (copy) Callback playbackFinishedCallback;       // 3
+
+@end
+
+#endif
---
+++ DemoAudioEngine/Source/ObjectiveCBindings_iOS_3.mm
@@ -0,0 +1,74 @@
+#include "JuceHeader.h"
+
+#if JUCE_IOS || JUCE_MAC
+
+#import "ObjectiveCBindings.h"
+#import "DemoAudioEngine.h"
+
+
+@interface DemoAudioEngineBindings() {
+    ScopedJuceInitialiser_GUI juceApp;  // This is needed for JUCE GUI
+    DemoAudioEngine audioEngine;
+}
+@end
+
+@implementation DemoAudioEngineBindings
+
+- (void) play: (NSString*) urlString
+{
+    audioEngine.play (urlString.UTF8String); 
+}
+
+- (void) stop
+{
+    audioEngine.stop();
+}
+
+- (void) pause
+{
+    audioEngine.pause();
+}
+
+- (void) resume
+{
+    audioEngine.resume();
+}
+
+- (void) setRoomSize: (float) roomSize
+{
+    audioEngine.setRoomSize (roomSize);
+}
+
+- (void) setLowpassCutoff: (float) cutoff
+{
+    audioEngine.setLowpassCutoff (cutoff);
+}
+
+- (void) setPlaybackDidFinish: (Callback) callback      // 3
+{
+    // Here we need to store the block/lambda in our Obj-C wrapper otherwise it will get deleted
+    self.playbackFinishedCallback = callback;
+    audioEngine.setPlaybackFinishedCallback(callback);
+}
+
+- (void) setWaveformComponentBounds: (CGRect) bounds                     // 2
+{
+    audioEngine.setWaveformComponentBounds (bounds.origin.x,
+                                            bounds.origin.y,
+                                            bounds.size.width,
+                                            bounds.size.height);
+}
+
+- (void) addWaveformComponentToView: (UIView*) viewToAttachTo            // 2
+{
+    audioEngine.addWaveformComponentToNativeParentView (viewToAttachTo);
+}
+
+- (void) removeWaveformComponentFromView                                // 2
+{
+    audioEngine.removeWaveformComponentFromNativeParentView();
+}
+
+@end
+
+#endif
