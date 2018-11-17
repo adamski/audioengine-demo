4cc2ef6d80f49b1d1a4945456d8d4739637ecdfc
Adam Wilson <adam@codegarden.co.uk>
2018-11-17 11:44:41 +0000

    Add macOS project!

---
+++ AudioEngineDemo-macOS/AudioEngineDemo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  AudioEngineDemo
+//
+//  Created by Adam Wilson on 13/11/2018.
+//  Copyright © 2018 com.acme. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
